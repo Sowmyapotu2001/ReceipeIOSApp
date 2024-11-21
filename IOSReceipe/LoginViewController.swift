f7fc8386717e20ccb6f549badfeab03a43dbc81d
SowmyaPotu <156160092+Sowmyapotu2001@users.noreply.github.com>
2024-11-21 10:22:59 -0600

    Add files via upload

---
+++ IOSReceipe/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSReceipe
+//
+//  Created by Sowmya potu on 11/12/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
